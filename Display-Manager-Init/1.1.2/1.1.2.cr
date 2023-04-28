c3ae1735c88cea4639c29a07840cee900984b7c9
Fulgurance <zohran.londais@gmail.com>
2023-04-28 09:41:22 +0100

    Added Display-Manager-Init

---
+++ Display-Manager-Init/1.1.2/1.1.2.cr
@@ -0,0 +1,20 @@
+class Target < ISM::Software
+    
+    def prepareInstallation
+        super
+
+        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/conf.d")
+
+        displayManagerData = <<-CODE
+        CHECKVT=7
+        DISPLAYMANAGER=""
+        CODE
+        fileWriteData("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/conf.d/display-manager.conf",displayManagerData)
+
+        prepareOpenrcServiceInstallation("#{workDirectoryPath(false)}/display-manager-setup.initd-r1","display-manager-setup")
+        prepareOpenrcServiceInstallation("#{workDirectoryPath(false)}/display-manager.initd-r5","	display-manager")
+        copyFile("#{workDirectoryPath(false)}/startDM-r1","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/bin/startDM")
+        runChmodCommand(["+x","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/bin/startDM"])
+    end
+
+end
